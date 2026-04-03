c02507ebab194dbb022752ec9b5a350a29ffd46a
parkhyomi <phyomi3@naver.com>
2026-04-03 15:49:21 +0900

    feat: SnackbarEvent 및 SnackbarMessageMapper에 태스크 삭제 및 수정 관련 메시지 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/feature/board/model/SnackbarEvent.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/feature/board/model/SnackbarEvent.kt
@@ -5,6 +5,10 @@ sealed interface SnackbarMessageType {
     data object TaskMoveFailed : SnackbarMessageType
     data object TaskAdded : SnackbarMessageType
     data object TaskAddFailed : SnackbarMessageType
+    data object TaskDeleted : SnackbarMessageType
+    data object TaskDeleteFailed : SnackbarMessageType
+    data object TaskUpdated : SnackbarMessageType
+    data object TaskUpdateFailed : SnackbarMessageType
 }
 
 data class SnackbarEvent(val id: Long, val type: SnackbarMessageType)
